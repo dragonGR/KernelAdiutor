ed51dac471ac445491e6cc4d7467d14f7131bcf1
Alex Tsanis <alextsanisbadlorg@gmail.com>
2018-01-02 01:09:03 +0000

    build.sh: Update path

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 
 ./gradlew clean assembleRelease
 
-[ -f app/app-release.apk ] || cp app/build/outputs/apk/app-release.apk app/app-release.apk
+[ -f app/app-release.apk ] || cp app/build/outputs/apk/release/app-release.apk app/app-release.apk
 [ -f app/app-release.apk ] && echo "APK Compiled" || exit 1
